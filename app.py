a1457197d6d80aa1078fc28c2ab79cb732b0bf02
kimiroo <43537287+kimiroo@users.noreply.github.com>
2025-08-19 15:50:25 +0900

    Changed default port to 28080

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from onvif_.monitor_events import ONVIFMonitor
 log = logging.getLogger('main')
 
 HOST = os.environ.get('HOST', '0.0.0.0')
-PORT = int(os.environ.get('PORT', '8080'))
+PORT = int(os.environ.get('PORT', '28080'))
 
 sio = socketio.AsyncServer(
     async_mode='asgi',
